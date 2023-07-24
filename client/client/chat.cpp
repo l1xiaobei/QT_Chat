e9d357ee83ad0888834c8e752cec204aa08e608a
xiaobei <l1xiaobei@qq.com>
2023-07-24 15:45:57 +0800

    ver 0.5.0

--- client/client/chat.cpp
+++ client/client/chat.cpp
@@ -12,6 +12,7 @@ chat::chat(QTcpSocket *s,QString name, QWidget *parent) :
 
     //接受服务器发送回的消息
     connect(socket, &QTcpSocket::readyRead, this, &chat::server_slot);
+
 }
 
 chat::~chat()
@@ -31,6 +32,7 @@ void chat::on_sendButton_clicked()
     QString timeStr = dateTime.toString("yyyy-MM-dd hh:mm:ss");//格式化时间
     ba.append(timeStr + "  " + ui->nameLine->text() + ": " + ui->sendLine->text());
     socket->write(ba);        //write()需要提供bytearray型的参数，所以要把qstring转换一下
+    ui->sendLine->clear();
 }
 
 void chat::server_slot()
@@ -42,19 +44,21 @@ void chat::server_slot()
 void chat::on_imageButton_clicked()
 {
     QString fileName = QFileDialog::getOpenFileName(this, tr("选择图片"), "*", tr("Images(*.png, *.jpg, *.jpeg, *.bmp, *.gif, *.webp, *.apng, *.sharpp)"));
-//    qDebug() << fileName;
-    QImage image(fileName); //创建图片对象
+    if(fileName.isEmpty()!=true)
+    {
+        QImage image(fileName); //创建图片对象
 //    image.scaled(110, 48, Qt::KeepAspectRatio,Qt::SmoothTransformation);  //缩放图片，但是保持图像纵横比
-    QByteArray imgBy;
+        QByteArray imgBy;
 
-    QBuffer imgBuf(&imgBy); //将图片数据写入到imgBy字节数组中
-    image.save(&imgBuf, "png");
+        QBuffer imgBuf(&imgBy); //将图片数据写入到imgBy字节数组中
+        image.save(&imgBuf, "png");
 
-    QByteArray ba;
-    QDateTime dateTime= QDateTime::currentDateTime();//获取系统当前的时间
-    QString timeStr = dateTime.toString("yyyy-MM-dd hh:mm:ss");//格式化时间
-    ba.append(timeStr + "  " + ui->nameLine->text() + ": ");   //!还没有发送给服务器哦!
-    QString htmlPath = QString("<img src=\"%1\" weight=\"220\" height=\"96\" />").arg(fileName); //resize图片大小
-                                                                                                 //   \ 用于转义 "
-    ui->chatText->insertHtml(htmlPath);
+        QByteArray ba;
+        QDateTime dateTime= QDateTime::currentDateTime();//获取系统当前的时间
+        QString timeStr = dateTime.toString("yyyy-MM-dd hh:mm:ss");//格式化时间
+        ba.append(timeStr + "  " + ui->nameLine->text() + ": ");   //!还没有发送给服务器哦!
+        QString htmlPath = QString("<img src=\"%1\" weight=\"220\" height=\"96\" />").arg(fileName); //resize图片大小
+        ui->chatText->append(ba);                                                                                         //   \ 用于转义 "
+        ui->chatText->insertHtml(htmlPath);
+    }
 }
