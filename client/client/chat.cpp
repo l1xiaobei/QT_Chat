ef019135ed0738c9a1226d3a65e14d6274575a7d
xiaobei <l1xiaobei@qq.com>
2023-07-24 14:45:26 +0800

    ver 0.4.1

--- client/client/chat.cpp
+++ client/client/chat.cpp
@@ -44,13 +44,17 @@ void chat::on_imageButton_clicked()
     QString fileName = QFileDialog::getOpenFileName(this, tr("选择图片"), "*", tr("Images(*.png, *.jpg, *.jpeg, *.bmp, *.gif, *.webp, *.apng, *.sharpp)"));
 //    qDebug() << fileName;
     QImage image(fileName); //创建图片对象
+//    image.scaled(110, 48, Qt::KeepAspectRatio,Qt::SmoothTransformation);  //缩放图片，但是保持图像纵横比
     QByteArray imgBy;
+
     QBuffer imgBuf(&imgBy); //将图片数据写入到imgBy字节数组中
+    image.save(&imgBuf, "png");
 
     QByteArray ba;
     QDateTime dateTime= QDateTime::currentDateTime();//获取系统当前的时间
     QString timeStr = dateTime.toString("yyyy-MM-dd hh:mm:ss");//格式化时间
     ba.append(timeStr + "  " + ui->nameLine->text() + ": ");   //!还没有发送给服务器哦!
-    QString htmlPath = QString("<img src=\"%1\"/>").arg(fileName);
+    QString htmlPath = QString("<img src=\"%1\" weight=\"220\" height=\"96\" />").arg(fileName); //resize图片大小
+                                                                                                 //   \ 用于转义 "
     ui->chatText->insertHtml(htmlPath);
 }
